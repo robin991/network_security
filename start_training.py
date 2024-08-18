035263743cb4cc56acb947edb25fb0a99ea3759e
robin991 <47486500+robin991@users.noreply.github.com>
2024-08-18 12:58:36 +0530

    training_pipeline,datatransformation, data_validation, data_evaluation, model_pusher, model_trainer.py, start_training.py  and training_pipelline—>init()

--- start_training.py
+++ start_training.py
@@ -1,16 +1,18 @@
-import os 
-import sys 
-from networksecurity.exception.exception import NetworkSecurityException
-from networksecurity.logger.logger import logging  
+import os
+import sys
 
-from networksecurity.pipeline.training_pipeline import TrainingPipeline  
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logger.logger import logging
+
+from networksecurity.pipeline.training_pipeline import TrainingPipeline
 
 
 def start_training():
     try:
-        pass 
-    except Exception as e :
+        model_training=TrainingPipeline()
+        model_training.run_pipeline()
+    except Exception as e:
         raise NetworkSecurityException(e,sys)
     
-if __name__ == "__main__":
+if __name__=='__main__':
     start_training()
